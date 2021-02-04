a89f5d6332845c89c60dfcd5b4cf2a00c130bc82
Joshua Tsao <joshuahj.tsao@gmail.com>
2021-02-04 10:12:58 +0800

    bs5 dosent work >:(((

--- bs5 beta/circuitscript.js
+++ bs5 beta/circuitscript.js
@@ -1,7 +1,4 @@
-			$(document).ready(function(){
-				$('[data-toggle="popover"]').popover({
-					placement : 'right',
-					trigger : 'hover, focus',
-					html : true,
-				});
-			}); 
+var popoverTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="popover"]'))
+var popoverList = popoverTriggerList.map(function (popoverTriggerEl) {
+	return new bootstrap.Popover(popoverTriggerEl)
+})
